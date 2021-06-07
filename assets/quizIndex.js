2d7a8729c787da2249542d7bb1032edb480bc9f4
mnlmrngl <64210587+mnlmrngl@users.noreply.github.com>
2021-06-07 07:17:45 +0200

    before changed html

--- assets/quizIndex.js
+++ assets/quizIndex.js
@@ -1,10 +1,8 @@
-function checkDisclaimerCookie() {
-    if (readCookie('disclaimerAccepted') == 'true')
-        console.log("ture")
-    else {
-        console.log("false")
-        document.location = '/disclaimer';
-    }
+if (readCookie('disclaimerAccepted') == 'true')
+    console.log("ture")
+else {
+    console.log("false")
+    document.location = '/disclaimer';
 }
 
 function readCookie(object) {
@@ -23,26 +21,75 @@ function readCookie(object) {
 }
 
 
-//Question
-var activeQuestion = document.querySelector('.question.question--active');
-var questionNumber = activeQuestion.dataset.number;
+// //Question
+// var activeQuestion = document.querySelector('.question.question--active');
+// var questionNumber = activeQuestion.dataset.number;
+// console.log(questionNumber);
+
+
+// //Next Question
+// document.querySelector('.question--active .question__next').addEventListener('click', function () {
+//     activeQuestion.classList.remove('question--active');
+//     activeQuestion = activeQuestion.nextElementSibling;
+//     activeQuestion.classList.add('question--active');
+
+//     questionNumber++;
+//     document.getElementById("question__num").textContent = questionNumber;
+//     console.log("next Question")
+//     console.log(questionNumber);
+
+// });
+
+// //Prev Question
+// document.getElementById('controls').firstElementChild.addEventListener('click', function () {
+//     if (activeQuestion.getAttribute('data-number').charAt(0) != 1) {
+//         activeQuestion.classList.remove('question--active');
+//         activeQuestion.previousElementSibling.classList.add('question--active');
+//         activeQuestion = activeQuestion.previousElementSibling;
+//         questionNumber--;
+//         document.getElementById("question__num").textContent = questionNumber;
+//     }
+// });
+
+//*************************************************************************************** */
+var activeQuestion = document.querySelector('.question--active');
+console.log(activeQuestion);
+var activeQuestionNumber = activeQuestion.dataset.number;
+console.log(activeQuestionNumber);
+
+var activeQuestionButtonNext = activeQuestion.firstElementChild.lastElementChild.lastElementChild;
 
 //Next Question
-document.querySelector('.question--active .question__next').addEventListener('click', function () {
+activeQuestionButtonNext.addEventListener('click', function () {
     activeQuestion.classList.remove('question--active');
-    activeQuestion.nextElementSibling.classList.add('question--active');
+    //new Active Question
+    activeQuestionNumber++;
+    console.log('New Active Question Number ' + activeQuestionNumber);
+
     activeQuestion = activeQuestion.nextElementSibling;
-    questionNumber++;
-    document.getElementById("question__num").textContent = questionNumber;
+    activeQuestion.classList.add('question--active');
+    console.log('New Active Question ')
+    console.log(activeQuestion);
+
+    activeQuestionButtonNext = activeQuestion.firstElementChild.lastElementChild.lastElementChild
+    console.log('Next Button')
+    console.log(activeQuestionButtonNext); //lastChild.lastChild;
+
 });
 
-//Prev Question
-document.getElementById('controls').firstElementChild.addEventListener('click', function () {
-    if (activeQuestion.getAttribute('data-number').charAt(0) != 1) {
-        activeQuestion.classList.remove('question--active');
-        activeQuestion.previousElementSibling.classList.add('question--active');
-        activeQuestion = activeQuestion.previousElementSibling;
-        questionNumber--;
-        document.getElementById("question__num").textContent = questionNumber;
-    }
-});
+document.getElementById('getNetxButton').addEventListener('click', function () {
+    console.log(activeQuestionButtonNext)
+})
+
+//******************************************************************************************* */
+
+
+
+// var activeQuestion = document.querySelector('.question--active');
+// console.log(activeQuestion)
+
+// var activeNumber = activeQuestion.dataset.number;
+// console.log(activeNumber)
+
+// var allQuestions = document.querySelectorAll('.question')
+// console.log(allQuestions)
