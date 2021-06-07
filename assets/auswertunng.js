5d40ebc76c9f2507979a71c749673630ed85ea1b
mnlmrngl <64210587+mnlmrngl@users.noreply.github.com>
2021-06-07 12:28:43 +0200

    show nogoes on result page

--- assets/auswertunng.js
+++ assets/auswertunng.js
@@ -1,10 +1,7 @@
 //stores solution in order
 var solution = [];
 
-console.log(document.cookie)
-
 var cookieArray = document.cookie.split(';').sort();
-console.log(cookieArray)
 
 for (var i = 0; i < cookieArray.length; i++) {
     var cookieElement = cookieArray[i];
@@ -17,4 +14,51 @@ for (var i = 0; i < cookieArray.length; i++) {
         solution.push(cookieElement.substring(start.length, cookieElement.length))
     }
 }
-console.log(solution)
+console.log('Solution ' + solution)
+
+//stores question numers of no-goes
+var noGoes = [];
+for (let i = 0; i < solution.length; i++) {
+    if (solution[i] == 4) {
+        noGoes.push(i + 1);
+    }
+}
+
+console.log('NoGoes ' + noGoes)
+
+
+//Show No Goes
+for (let i = 0; i < noGoes.length; i++) {
+    var span = document.createElement('span');
+    var img = document.createElement('img');
+    span.classList.add('no-go__item');
+    span.appendChild(img);
+    document.getElementById('no-go').appendChild(span);
+    img.setAttribute('src', '../assets/img/sideeffect_' + noGoes[i] + '.svg');
+
+    if (noGoes[i] == 1) {
+        span.setAttribute('data-sideeffect', 'Kopfschmerzen')
+    } else if (noGoes[i] == 2) {
+        span.setAttribute('data-sideeffect', 'Gewichtszunahme')
+    } else if (noGoes[i] == 3) {
+        span.setAttribute('data-sideeffect', 'Stimmungsveränderung')
+    } else if (noGoes[i] == 4) {
+        span.setAttribute('data-sideeffect', 'Depression')
+    } else if (noGoes[i] == 5) {
+        span.setAttribute('data-sideeffect', 'Verminderte Libido')
+    } else if (noGoes[i] == 6) {
+        span.setAttribute('data-sideeffect', 'Hautausschlag')
+    } else if (noGoes[i] == 7) {
+        span.setAttribute('data-sideeffect', 'Ausfallende Monatsblutung')
+    } else if (noGoes[i] == 8) {
+        span.setAttribute('data-sideeffect', 'Schmerzende BRunst')
+    } else if (noGoes[i] == 9) {
+        span.setAttribute('data-sideeffect', 'Wassereinlagerungen')
+    } else if (noGoes[i] == 10) {
+        span.setAttribute('data-sideeffect', 'Treockene Augen/Kontaktlinsen')
+    } else if (noGoes[i] == 11) {
+        span.setAttribute('data-sideeffect', 'Schlafstörungen')
+    } else if (noGoes[i] == 12)
+        span.setAttribute('data-sideeffect', 'Thrombose')
+
+}
