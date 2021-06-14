06a46afa67c361d759e0fd999384a2bf30f33d93
mnlmrngl <64210587+mnlmrngl@users.noreply.github.com>
2021-06-14 15:11:34 +0200

    cleand up disclaimerIndex.js

--- assets/js/disclaimerIndex.js
+++ assets/js/disclaimerIndex.js
@@ -4,11 +4,6 @@ var start = document.getElementById('start');
 //Start
 checkbox.addEventListener('click', function () {
     start.classList.toggle('button--disabled')
-    if (checkbox.checked == true) {
-        console.log(true)
-    } else {
-        console.log(false);
-    }
 });
 start.addEventListener("click", function () {
     if (document.getElementById('checkboxDisclaimer').checked == true) {
@@ -21,15 +16,13 @@ start.addEventListener("click", function () {
     }
 });
 
-
-
 //disclaimer from Quiz
 if (readCookie('disclaimerAccepted') == 'true') {
     var continueBtn = document.getElementById('continue');
-    //remove old stuff
+    //remove start buttons
     start.style.display = 'none';
     document.querySelector('label').style.display = 'none'
-    //show new stuff
+    //show new buttons
     document.getElementById('leave').style.display = 'inline-block';
     continueBtn.style.display = 'inline-block';
 
