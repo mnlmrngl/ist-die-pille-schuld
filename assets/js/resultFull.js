48296f8da1a87521403891f22cabad6279a6b2f7
mnlmrngl <64210587+mnlmrngl@users.noreply.github.com>
2021-06-17 09:30:15 +0200

    risiko eher eingehen

--- assets/js/resultFull.js
+++ assets/js/resultFull.js
@@ -8,7 +8,6 @@ var itemPointPlaceces = document.querySelectorAll('.highlight');
 
 var solution = [];
 var cookieArray = document.cookie.split(';').sort();
-console.log('coolieArray '+cookieArray)
 for (var i = 0; i < cookieArray.length; i++) {
     var cookieElement = cookieArray[i];
 
@@ -18,23 +17,40 @@ for (var i = 0; i < cookieArray.length; i++) {
     if (cookieElement.indexOf('a') == 0) {
         if (i < 10) {
             var start = 'a0' + i + '=';
-            console.log('<10 '+i)
-
-        } else{
+        } else {
             var start = 'a' + i + '=';
-            console.log('>10 '+i)
         }
         solution.push(cookieElement.substring(start.length, cookieElement.length))
     }
 }
 
-console.log(solution.length)
 
 for (let i = 0; i < itemPointPlaceces.length; i++) {
-    itemPointPlaceces[i].innerHTML = solution[i]-1 + '/3';
+    console.log(itemPointPlaceces.length)
+    itemPointPlaceces[i].innerHTML = solution[i] - 1 + '/3';
+    itemPointPlaceces[i].nextElementSibling.innerHTML = getExplenation(solution[i]-1)
 }
 
-document.getElementById('disclaimerBtn').addEventListener('click', function(){
+
+function getExplenation(risk) {
+    if (risk == 0)
+        return ' - du gehst das Risiko auf keinen Fall ein';
+    else if (risk == 1)
+        return ' - du gehst das Risiko ein bisschen ein';
+    else if (risk == 2)
+        return ' - du gehst das Risiko eher ein';
+    else if (risk == 3)
+        return ' - du gehst das Risiko auf jeden Fall ein';
+}
+
+document.getElementById('disclaimerBtn').addEventListener('click', function () {
     createCookie('currentQuestion', 'resultFull');
     document.location = '/disclaimer'
+});
+
+
+document.getElementById('downloadResult').addEventListener('click', function () {
+    const result = document.getElementById('resultToDownload');
+    console.log(result)
+    html2pdf().from(result).save();
 });
